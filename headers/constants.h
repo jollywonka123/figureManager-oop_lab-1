38e81d2c3545f1c439089f920fb44ca9d9c77cd8
Добра Виктор <81097869+jollywonka123@users.noreply.github.com>
2025-04-08 01:51:04 +0300

    Add files via upload

---
+++ headers/constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+enum Option {
+    DEFAULT = -1,
+    EXIT,
+    ADD_FIGURE,
+    OUTPUT_PARAM,
+    OUTPUT_PERIM,
+    PRINT_PERIM_SUM,
+    SORT_PERIM_ASC,
+    DELETE_BY_NUM,
+    DELETE_LARGER_PERIM,
+};
+
+#endif //CONSTANTS_H
