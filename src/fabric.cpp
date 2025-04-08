fba0133d06328d171bca8dbcf5999c6607c558dd
Добра Виктор <81097869+jollywonka123@users.noreply.github.com>
2025-04-09 00:24:04 +0300

    Update fabric.cpp

--- src/fabric.cpp
+++ src/fabric.cpp
@@ -1,16 +1,18 @@
 #include "../headers/fabric.h"
 #include "../headers/inputValidator.h"
 #include "string"
+#include "../headers/constants.h"
 
 std::shared_ptr<Figure> CircleFabric::createFigure() {
-    double x, y, rad;
+    double rad;
+    Dot centre;
     std::shared_ptr<Figure> figure = nullptr;
     std::string creationName;
     std::string prompt = "Enter data for circle to create object - (Name X Y Radius): ";
-    InputValidator::getInput<std::string, double, double, double>(prompt, &creationName, &x, &y, &rad);
+    InputValidator::getInput<std::string, double, double, double>(prompt, &creationName, &centre.x, &centre.y, &rad);
     if (InputValidator::isError == false)
         try {
-            figure = std::make_shared<Circle>(creationName, x, y, rad);
+            figure = std::make_shared<Circle>(creationName, centre, rad);
         } catch (std::invalid_argument& e) {
             std::cout << "Error: " << e.what() << std::endl;
         }
@@ -18,14 +20,14 @@ std::shared_ptr<Figure> CircleFabric::createFigure() {
 }
 
 std::shared_ptr<Figure> RectangleFabric::createFigure() {
-    double xLeft, yUpper, xRight, yLower;
+    Dot uLeft, lRight;
     std::string creationName;
     std::shared_ptr<Figure> figure = nullptr;
     std::string prompt = "Enter data for rectangle to create object - (Name LeftX UpperY RightX LowerY): ";
-    InputValidator::getInput<std::string, double, double, double, double>(prompt, &creationName, &xLeft, &yUpper, &xRight, &yLower);
+    InputValidator::getInput<std::string, double, double, double, double>(prompt, &creationName, &uLeft.x, &uLeft.y, &lRight.x, &lRight.y);
     if (InputValidator::isError == false)
         try {
-            figure = std::make_shared<Rectangle>(creationName, xLeft, yUpper, xRight, yLower);
+            figure = std::make_shared<Rectangle>(creationName, uLeft, lRight);
         } catch (std::invalid_argument& e) {
             std::cout << "Error: " << e.what() << std::endl;
         }
@@ -33,14 +35,14 @@ std::shared_ptr<Figure> RectangleFabric::createFigure() {
 }
 
 std::shared_ptr<Figure> TriangleFabric::createFigure() {
-    double x1, y1, x2, y2, x3, y3;
+    Dot c1, c2, c3;
     std::string creationName;
     std::shared_ptr<Figure> figure = nullptr;
     std::string prompt = "Enter data for triangle to create object - (Name 3XY for each coord., split by space): ";
-    InputValidator::getInput<std::string, double, double, double, double, double, double>(prompt, &creationName, &x1, &y1, &x2, &y2, &x3, &y3);
+    InputValidator::getInput<std::string, double, double, double, double, double, double>(prompt, &creationName, &c1.x, &c1.y, &c2.x, &c2.y, &c3.x, &c3.y);
     if (InputValidator::isError == false)
         try {
-            figure = std::make_shared<Triangle>(creationName, x1, x2, x3, y1, y2, y3);
+            figure = std::make_shared<Triangle>(creationName, c1, c2, c3);
         } catch (std::invalid_argument& e) {
             std::cout << "Error: " << e.what() << std::endl;
         }
